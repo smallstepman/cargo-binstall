46f486b155d9a538ad9d237f7d0e802dcca39511
Jiahao XU <Jiahao_XU@outlook.com>
2023-08-08 20:58:53 +1000

    feat: Verify cksum of crate tarball from cargo registry
    Fixed #1183
    
    Since the crate tarball could be downloaded from a different set of
    servers than where the cargo registry is hosted, verifying the checksum
    is necessary to verify its integrity.
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- crates/binstalk/src/helpers.rs
+++ crates/binstalk/src/helpers.rs
@@ -8,7 +8,7 @@ pub mod signal;
 pub mod target_triple;
 pub mod tasks;
 
-pub use binstalk_downloader::{download, gh_api_client};
+pub use binstalk_downloader::{bytes, download, gh_api_client};
 
 pub fn is_universal_macos(target: &str) -> bool {
     ["universal-apple-darwin", "universal2-apple-darwin"].contains(&target)
